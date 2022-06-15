71dd5ecb9656e96d50a0c4b83abe2e168759ecc6
lovexiaoxia <aadyx2007@163.com>
2022-06-15 17:34:26 +0800

    完善功能
    Signed-off-by: lovexiaoxia <aadyx2007@163.com>

--- MKNBHplugApp/Classes/Expand/Manager/ExcelManager/MKNBHExcelDataManager.m
+++ MKNBHplugApp/Classes/Expand/Manager/ExcelManager/MKNBHExcelDataManager.m
@@ -244,12 +244,12 @@ static NSString *const defaultKeyValueString = @"value:";
     MKNBHExcelCell *keepAliveCell = [sheet getCellWithColumn:@"B" row:9 error:nil];
     NSString *keepAlive = [SafeStr(keepAliveCell.stringValue) stringByReplacingOccurrencesOfString:defaultKeyValueString withString:@""];
     keepAlive = [keepAlive stringByReplacingOccurrencesOfString:@" " withString:@""];
-    NSInteger keepValue = [keepAlive integerValue];
-    if (keepValue < 10 || keepValue > 120) {
-        //默认值
-        keepValue = 60;
-    }
-    [dic setObject:@(keepValue) forKey:@"keepAlive"];
+//    NSInteger keepValue = [keepAlive integerValue];
+//    if (keepValue < 10 || keepValue > 120) {
+//        //默认值
+//        keepValue = 60;
+//    }
+    [dic setObject:keepAlive forKey:@"keepAlive"];
     //MQTT Username
     MKNBHExcelCell *usernameCell = [sheet getCellWithColumn:@"B" row:10 error:nil];
     NSString *username = [SafeStr(usernameCell.stringValue) stringByReplacingOccurrencesOfString:defaultKeyValueString withString:@""];
@@ -605,12 +605,12 @@ static NSString *const defaultKeyValueString = @"value:";
     //Keep Alive
     MKNBHExcelCell *keepAliveCell = [sheet getCellWithColumn:@"B" row:9 error:nil];
     NSString *keepAlive = [SafeStr(keepAliveCell.stringValue) stringByReplacingOccurrencesOfString:defaultKeyValueString withString:@""];
-    NSInteger keepValue = [keepAlive integerValue];
-    if (keepValue < 10 || keepValue > 120) {
-        //默认值
-        keepValue = 60;
-    }
-    [dic setObject:@(keepValue) forKey:@"keepAlive"];
+//    NSInteger keepValue = [keepAlive integerValue];
+//    if (keepValue < 10 || keepValue > 120) {
+//        //默认值
+//        keepValue = 60;
+//    }
+    [dic setObject:keepAlive forKey:@"keepAlive"];
     //MQTT Username
     MKNBHExcelCell *usernameCell = [sheet getCellWithColumn:@"B" row:10 error:nil];
     NSString *username = [SafeStr(usernameCell.stringValue) stringByReplacingOccurrencesOfString:defaultKeyValueString withString:@""];
